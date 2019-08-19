319b95de25629d89340052acec5f1592a23c6410
Vladimir Remeniuk <vlremenyuk@yandex.ru>
2019-08-19 21:51:27 +0300

    changed once more just to check if it's working

--- src/App.js
+++ src/App.js
@@ -95,7 +95,6 @@ class App extends React.Component {
         console.log('this.state.messages:', this.state.messages);
         return (
             <div className="app">
-				dzzzhopppa
 				<RoomList 
 					roomId={this.state.roomId}
 					subscribeToRoom={this.subscribeToRoom}
